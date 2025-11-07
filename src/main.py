f5a9bb02b0dc70e04f541fdcb7315549f0a85d13
anagha-285 <saraswathyanagha20@gmail.com>
2025-11-07 09:50:12 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from train import train
+from evaluate import evaluate
+
+if __name__ == "__main__":
+    print("🎧 Starting DeepACE2.0 Pipeline")
+    train()
+    print("\n🔍 Evaluating Model Performance")
+    evaluate()
